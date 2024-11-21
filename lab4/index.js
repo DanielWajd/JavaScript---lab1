7e72dd91b9471fe03426638d73554b351848f506
DanielWajd <wajdowiczdaniel@gmail.com>
2024-11-21 09:18:09 +0100

    Cm4_4

--- lab4/index.js
+++ lab4/index.js
@@ -57,6 +57,16 @@ app.get('/math/power/:base/:exponent', (req, res) => {
   res.json(result);
 });
 
+let categories = ['funnyJoke', 'lameJoke'];
+let funnyJoke = [
+    {'joke': 'Dlaczego komputer poszedł do lekarza?', 'response': 'Bo złapał wirusa!'},
+    {'joke': 'Dlaczego komputer nie może być głodny?', 'response': 'Bo ma pełen dysk!'},
+    {'joke': 'Co mówi jeden bit do drugiego?', 'response': 'Trzymaj się, zaraz się przestawiamy!'}
+];
+let lameJoke = [
+    {'joke': 'Dlaczego programiści preferują noc?', 'response': 'Bo w nocy jest mniej bugów!'},
+    {'joke': 'Jak nazywa się bardzo szybki programista?', 'response': 'Błyskawiczny kompilator!'}
+];
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
